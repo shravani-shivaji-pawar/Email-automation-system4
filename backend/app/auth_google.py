dd1ea1d50adf6d5904ed42f3acfa28847556d340
Shravani <shravanipawar7704@gmail.com>
2026-08-18 23:53:02 +0530

    Update email automation project

--- backend/app/auth_google.py
+++ backend/app/auth_google.py
@@ -19,6 +19,7 @@ STATE_SECRET = os.getenv("OAUTH_STATE_SECRET", "dev-insecure-change-me")
 
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
     "openid",
